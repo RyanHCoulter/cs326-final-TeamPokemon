61991e1fe657d4394bea42aa9985515f256d4609
MiniShark27 <47121248+MiniShark27@users.noreply.github.com>
2020-12-04 18:20:06 -0500

    Remove Commented Code and Consele Log

--- client/recruits.js
+++ client/recruits.js
@@ -1,8 +1,6 @@
 import * as f from "./fetch.js";
 window.onload = async () => {
-   // const pokemons =await Promise.all(['pikachu','geodude','charmander'].map(enemy=>f.pokeInfo(enemy)));
     const pokemons =await f.getPokemon();
-    console.log(pokemons);
     const history = document.getElementById("history");
     history.innerHTML = '';
     pokemons.forEach(pokemon=>{
@@ -31,8 +29,6 @@ window.onload = async () => {
 
         history.appendChild(row);
     })
-
-
 }
 
 
